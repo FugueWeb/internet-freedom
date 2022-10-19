711704f9bf5ef40d9d272352a525ec1762be03b1
FugueWeb <info@fugueweb.com>
2022-10-18 21:42:09 -0400

    added blank env files

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+    production: true,
+    BLOCK_NATIVE_KEY: "",
+    ETHERSCAN_API: "",
+    INFURA_ID: "",
+    INFURA_OGOERLI_KEY:""
+  };
---
+++ src/environments/environment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+    production: false,
+    BLOCK_NATIVE_KEY: "",
+    ETHERSCAN_API: "",
+    INFURA_ID: "",
+    INFURA_OGOERLI_KEY:""
+  };
